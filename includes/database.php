e50fad9e036c9997a48396a9a27dd01296aebf18
Oscar <yo@gmail.com>
2023-02-24 17:58:02 -0600

    Quite un codigo que estaba comentado

--- includes/database.php
+++ includes/database.php
@@ -9,29 +9,4 @@ if (!$db) {
     exit;
 }
 
-use Model\ActiveRecord;
-
-/*class Database {
-
-    public $host = 'localhost';
-    public $usuario = 'root';
-    public $contrasena = 'password';
-    public $nombreBD = 'appsalon';
-    
-    public static function crearConexion() {
-        $dsn = 'mysql:host=' . self::$host . ';dbname=' . self::$nombreBD . ';charset=utf8mb4';
-        
-        $opciones = [
-            PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
-            PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
-            PDO::ATTR_EMULATE_PREPARES => false,
-        ];
-        try {
-            $pdo = new PDO($dsn, self::$usuario, self::$contrasena, $opciones);
-            return $pdo;
-        } catch (PDOException $e) {
-            throw new PDOException($e->getMessage(), (int)$e->getCode());
-        }
-    } 
-}*/
 
